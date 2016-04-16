338b129e6ac9c181389e0137ac1308ca612c4b4f
PlebeianStudio <mathiasnat@gmail.com>
2016-04-16 06:40:56 +0200

    Added some basic unit behaviour

---
+++ Assets/Scripts/HitPoints.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HitPoints : MonoBehaviour {
+    public int Hp = 10;
+
+
+	// Update is called once per frame
+	void Update () {
+        if (Hp <= 0)
+            Kill();
+	}
+
+    void Kill() {
+        GameObject.Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum UnitBehaviour {Enemy, Minion }
+
+[RequireComponent(typeof(HitPoints))]
+public class Unit : MonoBehaviour {
+
+    public UnitBehaviour behaviour = UnitBehaviour.Enemy;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        switch (behaviour) {
+            case UnitBehaviour.Enemy:
+                EnemyUpdate();
+                break;
+            case UnitBehaviour.Minion:
+                MinionUpdate();
+                break;
+        }
+	
+	}
+
+    void EnemyUpdate() {
+        //disbale so we don't collide with our selfs
+        GetComponent<Collider2D>().enabled = false;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, 1f);
+        Debug.Log(hit.collider);
+        Debug.DrawRay(transform.position, Vector2.left, Color.green);
+        if (hit.collider == null) {
+            //move to the left
+            transform.Translate(Vector3.left * Time.deltaTime);
+        }
+        else if (hit.collider.tag == "Enemy") {
+            //we are behind a friend, wait
+        }
+        else if (hit.collider.tag == "Minion") {
+            //we are faceing an enemy, attack
+
+        }
+        else if (hit.collider.tag == "TowerBlock") {
+            //we are facking the tower, attck it!
+            hit.transform.GetComponent<HitPoints>().Hp--;
+        }
+        //re anable so others can collide with us :)
+        GetComponent<Collider2D>().enabled = true;
+    }
+
+        void MinionUpdate() {
+        //disbale so we don't collide with our selfs
+        GetComponent<Collider2D>().enabled = false;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, 1f);
+        Debug.Log(hit.collider);
+        Debug.DrawRay(transform.position, Vector2.right, Color.green);
+        if (hit.collider == null) {
+            //move to the left
+            transform.Translate(Vector3.right * Time.deltaTime);
+        }
+        else if (hit.collider.tag == "Enemy") {
+            //we are faceing an enemy, attack
+        }
+        else if (hit.collider.tag == "Minion") {
+            //we are behind a friend, wait
+
+        }
+
+        //re anable so others can collide with us :)
+        GetComponent<Collider2D>().enabled = true;
+    }
+}
